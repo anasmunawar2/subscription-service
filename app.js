1f2aea9be2101179c0ef7202775d506e7860ae44
anasmunawar2 <anasmunawar2@gmail.com>
2025-08-08 21:44:01 +0500

    Implement MongoDB connection and update dependencies

--- app.js
+++ app.js
@@ -3,21 +3,24 @@ import { PORT } from "./config/env.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("", subscriptionRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the subscription service!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(
     `Subscription service is running on port http://localhost:${PORT}`
   );
+
+  await connectToDatabase();
 });
 
 export default app;
