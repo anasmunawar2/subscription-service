23efaaba9a53629933230cac9dd5fa953f577962
anasmunawar2 <anasmunawar2@gmail.com>
2025-08-25 21:30:19 +0500

    feat: enhance subscription workflow by integrating reminder trigger and updating route

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,5 +1,6 @@
 import Subscription from "../models/subscription.model.js";
 import { workflowClient } from "../config/upstash.js";
+import { SERVER_URL } from "../config/env.js";
 
 export const createSubscription = async (req, res, next) => {
   try {
@@ -8,12 +9,22 @@ export const createSubscription = async (req, res, next) => {
       user: req.user._id,
     });
 
-    await workflowClient.trigger({
-      url: `${SERVER_URL}`,
+    const { workflowRunId } = await workflowClient.trigger({
+      url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: subscription.id,
+      },
+      headers: {
+        "content-type": "application/json",
+      },
+      retries: 0,
     });
-    res.status(201).json({ success: true, data: subscription });
-  } catch (error) {
-    next(error);
+
+    res
+      .status(201)
+      .json({ success: true, data: { subscription, workflowRunId } });
+  } catch (e) {
+    next(e);
   }
 };
 
